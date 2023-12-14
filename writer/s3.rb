d278dab1bdcbb69bdd9a3cd054cb07944c986a62
rexmadden <rexmadden@gmail.com>
2023-12-14 17:20:23 -0500

    Explicitly add S3 region

--- writer/s3.rb
+++ writer/s3.rb
@@ -7,12 +7,14 @@ class Writer < WriterBase
   S3_CLIENT = Aws::S3::Client.new({
     access_key_id: ENV.fetch('S3_KEY'),
     secret_access_key: ENV.fetch('S3_SECRET'),
-    region: ENV.fetch('S3_REGION'),
   })
 
   def stream_to(filepath)
     @logger.info "begin #{filepath}"
-    S3_CLIENT.put_object(bucket: ENV.fetch('S3_BUCKET'), key: filepath, body: @io)
+    S3_CLIENT.put_object(region: ENV.fetch('S3_REGION'),
+                         bucket: ENV.fetch('S3_BUCKET'),
+                         key: filepath,
+                         body: @io)
     @logger.info "end #{filepath}"
   end
 
