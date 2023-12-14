bb0a9138e504441750b16760ae8f2aeb175dcd9f
rexmadden <rexmadden@gmail.com>
2023-12-14 17:11:42 -0500

    Use standard s3 sdk functions for putting object

--- writer/s3.rb
+++ writer/s3.rb
@@ -4,17 +4,14 @@ require_relative './base.rb'
 
 class Writer < WriterBase
 
-  S3_BUCKET_OBJECTS = AWS::S3.new({
+  S3_CLIENT = AWS::S3::Client.new({
     access_key_id: ENV.fetch('S3_KEY'),
     secret_access_key: ENV.fetch('S3_SECRET'),
-  }).buckets[ENV.fetch('S3_BUCKET')].objects
+  })
 
   def stream_to(filepath)
     @logger.info "begin #{filepath}"
-    S3_BUCKET_OBJECTS[filepath].write(
-      @io,
-      estimated_content_length: 1 # low-ball estimate; so we can close buffer by returning nil
-    )
+    S3_CLIENT.put_object(bucket: ENV.fetch('S3_BUCKET'), key: filepath, body: @io)
     @logger.info "end #{filepath}"
   end
 
